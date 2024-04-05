558816f30fad7498f22714de49d98e7493b49c43
Phong <phongln@Phongs-MBP.Home>
2024-04-05 15:13:27 +0700

    fix find account to DB

--- src/process/accountManager.ts
+++ src/process/accountManager.ts
@@ -3,7 +3,7 @@ import { ctx } from "../processor";
 import { hexToNativeAddress } from "../util/util";
 
 const importAccount = async (publicKey: string) => {
-  const idExist = ctx.store.findOne(Account, {
+  const idExist = await ctx.store.findOne(Account, {
     where:{
       id: publicKey
     }
